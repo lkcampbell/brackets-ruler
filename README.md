b60a42cd0329b2bea74c0ed579db3d5cfcd5e556
Lance Campbell <lkcampbell@gmail.com>
2013-04-28 10:44:09 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ruler for Brackets
+An extension for [Brackets](https://github.com/adobe/brackets/) that adds
+an adjustable column ruler to the editor.
+
+**WARNING:** This extension is in experimental mode and is not functional
+at all. I am putting it on GitHub purely for my own convenience.  Please
+do **not** install it or use it until I have made an announcement on the
+Bracket-Dev Google Group that it is ready to use.
+
+### How to Install
+1. Select **Brackets > File > Install Extension...**
+2. Paste https://github.com/lkcampbell/brackets-ruler
+into Extension URL field.
+3. Click on the **Install** button.
+
+### How to Use Ruler
+Toggle the extension with **Brackets > View > Toggle Ruler** or use the
+shortcut key which is **Ctrl-Alt-R** on Windows and **Command-Alt-R** on Mac.
+
+### Roadmap
+* Create ruler
+* Create column guide on ruler click
+* Make column guide draggable
+* Option for word wrap at column guide
+
+### License
+MIT-licensed -- see `main.js` for details.
+
+### Compatibility
+Brackets Sprint 22 or later.
---
+++ main.js
@@ -0,0 +1,69 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Lance Campbell. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, regexp: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    // --- Required modules ---
+    var PreferencesManager  = brackets.getModule("preferences/PreferencesManager"),
+        Menus               = brackets.getModule("command/Menus"),
+        CommandManager      = brackets.getModule("command/CommandManager");
+    
+    // --- Constants ---
+    var COMMAND_NAME    = "Toggle Ruler",
+        COMMAND_ID      = "lkcampbell.toggle-ruler",
+        SHORTCUT_KEY    = "Ctrl-Alt-M";
+    
+    // --- Local variables ---
+    var _defPrefs   = { enabled: false },
+        _prefs      = PreferencesManager.getPreferenceStorage(module, _defPrefs),
+        _viewMenu   = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+
+    // --- Event handlers ---
+    function _toggleRuler() {
+        var command = CommandManager.get(COMMAND_ID);
+        
+        command.setChecked(!command.getChecked());
+        _prefs.setValue("enabled", command.getChecked());
+        alert("Ruler implementation goes here.");
+    }
+    
+    // Update ruler when changing editors
+    
+    // Load ruler HTML and style sheet
+    
+    // Register command
+    CommandManager.register(COMMAND_NAME, COMMAND_ID, _toggleRuler);
+    
+    // Add to View menu
+    if (_viewMenu) {
+        _viewMenu.addMenuItem(COMMAND_ID, SHORTCUT_KEY);
+    }
+    
+    // Apply preferences
+    CommandManager.get(COMMAND_ID).setChecked(_prefs.getValue("enabled"));
+});
